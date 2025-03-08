1571cdf68f553a2cbd2da772f0faefcef32c874d
vidishraj <vidishraj@gmail.com>
2025-03-08 10:28:53 +0530

    refacto: Refact to use context instead

--- src/components/StoryBox.tsx
+++ src/components/StoryBox.tsx
@@ -3,15 +3,17 @@ import styles from "./StoryBox.module.scss";
 interface StoryBoxProps {
   imageSrc: string;
   userName: string;
+  viewed: boolean;
 }
 
 const StoryBox: React.FC<StoryBoxProps> = (props) => {
   // A story box will be of fix height and width
   // The container will move and scale accordingly
-  const { imageSrc, userName } = props;
+  const { imageSrc, userName, viewed } = props;
   return (
     <div className={styles.storyBox}>
       <img
+        style={{ border: viewed ? "1px solid grey" : "1px solid red" }}
         className={styles.profilePicture}
         src={imageSrc}
         alt="profile image"
